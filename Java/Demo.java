800a8fdc4f8be8a4c0145f65dd66f7090df9c001
Sethu-Siva <sivarocks742@gmail.com>
2023-02-10 22:21:45 +0530

    Save

---
+++ Java/Demo.java
@@ -0,0 +1,19 @@
+class Demo
+{
+    public static void main(String args[])
+    {
+        int a = 10, b = 20, sum;
+        float d = 10.50f, e = 20.10f, total;
+        sum = a + b;
+        total = d + e;
+        System.out.println("Welcome To Java");
+        System.out.println("-------------------------");
+        System.out.println("A value : " + a);
+        System.out.println("B value: " + b);
+        System.out.println("Sum of A + B : " + sum);
+        System.out.println("-------------------------");
+        System.out.println("D value : " + d);
+        System.out.println("E value: " + e);
+        System.out.println("Total of D + E : " + total);
+    }
+}
