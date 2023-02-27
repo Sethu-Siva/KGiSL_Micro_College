77682165fb77072ab2debeba7099eb86fcf804a9
Sethu-Siva <sivarocks742@gmail.com>
2023-02-27 12:38:02 +0530

    save

--- JavaScript/Demo/Demo.js
+++ JavaScript/Demo/Demo.js
@@ -4,5 +4,20 @@ function msg()
 }
 function content()
 {
-    document.write("Welcome to Js");
+    document.write("<h1>Welcome to Js</h1>");
+}
+function error(e)
+{
+    try{
+        var a = 10;
+        var b = 20;
+        var c = a + e;
+        document.write(a + " + " + e + " = " + c + "<br>");
+        document.write(a + " + " + b + " = " + (a+b));
+    }
+    catch(err) 
+    {
+        console.log("Error : " , err);
+        document.write("Error : " , err);
+    }
 }
