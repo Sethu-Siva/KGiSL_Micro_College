4246597c84872492d9b3409bf939b16d58b031d1
Sethu-Siva <sivarocks742@gmail.com>
2023-03-03 09:36:26 +0530

    Save JS

---
+++ JavaScript/Date Example/Date.js
@@ -0,0 +1,10 @@
+var d = new Date();
+console.log(d);
+console.log("Date          :",d.getDate());
+console.log("Day in Week   :",d.getDay());
+console.log("Month         :",d.getMonth());
+console.log("Full Year     :",d.getFullYear());
+console.log("Hour          :",d.getHours());
+console.log("Minutes       :",d.getMinutes());
+console.log("Seconds       :",d.getSeconds());
+console.log("Milli Seconds :",d.getMilliseconds());
---
+++ JavaScript/Objects Example/Objects.js
@@ -0,0 +1,187 @@
+function myfunc()
+{
+    var SideBanner = {
+        Movie_Name : "Ponniyin Selvan: I",
+        Film_Poster : ["https://upload.wikimedia.org/wikipedia/en/c/c3/Ponniyin_Selvan_I.jpg","Theatrical release poster"],
+        Directed_by : "Mani Ratnam",
+        Screenplay : ["Mani Ratnam","Elango Kumaravel"],
+        Based_on : ["Ponniyin Selvan","by Kalki Krishnamurthy"],
+        Produced_by : ["Mani Ratnam","Subaskaran Allirajah"],
+        Starring : ["Vikram","Aishwarya Rai Bachchan","Jayam Ravi","Karthi","Trisha","Jayaram","Aishwarya Lekshmi","Sobhita Dhulipala","Prabhu","R. Sarathkumar","Vikram Prabhu","Prakash Raj","Rahman","R. Parthiban"],
+        Narrated_by : "Kamal Haasan",
+        Cinematography : "Ravi Varman",
+        Edited_by : "A. Sreekar Prasad",
+        Music_by : "A. R. Rahman",
+        Production_companies : ["Madras Talkies","Lyca Productions"],
+        Distributed_by : ["Red Giant Movies (Tamil Nadu)","Sri Venkateswara Creations (Andhra Pradesh & Telangana)","K Senthil (Karnataka)","Sree Gokulam Movies (Kerala)","Pen India Limited (North India)","Lyca Productions in association with Tentkotta (Overseas)"],
+        Release_date : "30 September 2022",
+        Running_time : "167 minutes[1]",
+        Country : "India",
+        Language : "Tamil",
+        Budget : "See production",
+        Box_office : "est. ₹500.6 crore"
+    }
+
+    var content = {
+        main : ["Ponniyin Selvan: I (transl. The Son of Ponni) is a 2022 Indian Tamil-language epic historical action adventure film directed by Mani Ratnam, who co-wrote it with Elango Kumaravel and B. Jeyamohan. Produced by Ratnam and Subaskaran Allirajah under Madras Talkies and Lyca Productions, it is the first of two cinematic parts based on Kalki Krishnamurthy's 1955 novel, Ponniyin Selvan. The film stars an ensemble cast including Vikram, Aishwarya Rai Bachchan, Jayam Ravi, Karthi, Trisha, Jayaram, Aishwarya Lekshmi, Sobhita Dhulipala, Prabhu, R. Sarathkumar, Vikram Prabhu, Prakash Raj, Rahman, R. Parthiban and Lal.[4] The music was composed by A. R. Rahman, with cinematography by Ravi Varman, editing by A. Sreekar Prasad, and production design by Thota Tharani. Ponniyin Selvan: I dramatizes the early life of Chola prince Arulmozhi Varman, who would become the renowned emperor Rajaraja I (947–1014).[5]",
+
+        "Ever since its publication, a film adaptation of Ponniyin Selvan had been explored by several Tamil filmmakers, including an attempt by M. G. Ramachandran in the late 1950s; however, it never materialized due to financial constraints. Decades later, Ratnam attempted to adapt the novel in the late-1980s and early-2010s but was unsuccessful. Calling it his //dream project//, Ratnam revived the effort in January 2019, after Lyca agreed to fund the film. Following several changes in cast and crew, production of Ponniyin Selvan began in December 2019 and concluded in September 2021, halting twice due to the COVID-19 pandemic. The film was shot in various locations across India, with a few sequences in Thailand. It was originally intended to be a single film but was split into two parts.",
+
+        "Ponniyin Selvan: I was released in theatres worldwide on 30 September 2022 in standard and IMAX formats, where it received acclaim from film critics, who praised the direction, cast performances, musical score, visuals and faithfulness to the novel. Breaking several box-office records, the film grossed more than ₹500 crore (US$63 million) and became the highest-grossing Tamil film of 2022, the fourth-highest-grossing Tamil film of all time and the 15th highest-grossing Indian film of all time. It received six nominations at the 16th Asian Film Awards, including Best Film."],
+
+        plot : ["During 10th century in Thanjavur, the Chola dynasty rules prosperously in South India, under the reign of Emperor Sundara Chozhar, whose sons Aditha Karikalan and Arulmozhi Varman aka Ponniyin Selvan are heading their conquests of Kanchi and Lanka, respectively for the empire. Sensing something is amiss with the courtiers, Aditha Karikalan sends his friend, the Vaanar Clan's prince, Vallavaraiyan Vandiyadevan to investigate and relay the news to his sister Princess Kundavai and Sundara Chozhar subsequently. Vandiyadevan reaches the fortress of Kadamboor and overhears the conspiracy devised by the treasurer and minister of finance, Periya Pazhuvettaraiyar and other ministers to stop Aditha Karikalan from ascending the throne and install his uncle Madhurantaka Chola on it.",
+
+        "Vandiyadevan had frequent meetings with a Vaishnavite Azhwarkkadiyan Nambi, whom he spots at the fortress spying on the meeting. After escaping from the fortress, Vandiyadevan confronts Nambi on a boat, who reveals that he was in the fortress to meet his foster sister Nandini, who mysteriously disappeared from the house during his absence and later married Periya Pazhuvettaraiyar, who is much older than her. After learning this, Vandiyedavan meets Pazhuvettaraiyar's wife Nandini, who tells him to meet her at her building and gives her ring to him for entry into the royal palace when he expresses that he wants to deliver Nambi's message to her. Vandiyedavan meets the fort's chief-in-charge Chinna Pazhuvettaraiyar and reveals to Sundara Chozhar about the conspirators, but Chinna Pazhuvettaraiyar, who is Pazhuvettaraiyar's younger brother intervenes and tries to capture Vandiyedavan, who manages to escape from them and meets Aditha Karikalan's sister Kundavai through Nandini, who orders him to bring her reply first to her before reaching Aditha Karikalan.",
+        
+        "After learning about the conspiracy, Kundavai (on whom Vandiyadevan begins to develop feelings) tells him to bring Ponniyin Selvan from Lanka. After winning the battle in Kanchi, Aditha Karikalan is told to return to Thanjavur but refuses as Nandini is present in Thanjavur. It is revealed that Aditha Karikalan was in love with Nandini, but the royalty refused to accept a poor orphan as the prospective queen of the Chola empire and banished her from the city. After years, Aditha Karikalan met Nandini tending to his enemy Veerapandiyan, who was the king of Pandya dynasty with whom she fell in love after being banished. In a fit of rage, Aditha Karikalan beheaded Veerapandiyan causing a depressed Nandini to swear vengeance against his family. Presently, Aditha Karikalan knows that Nandini married Periya Pazhuvettaraiyar just to avenge her past.",
+        
+        "After winning the war against Raja Mahinda in Lanka, Ponniyin Selvan meets Vandiyadevan, who relays messages for him from Kundavai and Vaanathi (Ponniyin Selvan's love interest and Princess of Kodumbalur) by reaching Lanka with the help of a rowing woman Poonguzhali. Nambi also arrives in Lanka and is revealed to be a spy of the Prime minister Aniruddha Brahmarayar. After a face-off with Nandini in Thanjavur, Kundavai then meets Sundara Chozhar and tells him about Periya Pazhuvettaraiyar's conspiracies. The next day, Pazhuvettaraiyar and the ministers manipulate Sundara Chozhar to order a convoy to bring Ponniyin Selvan back to Thanjavur. Meanwhile, Kundavai is asked by Sundara Chozhar to bring Aditha Karikalan to Thanjavur, as it would not be appropriate for the crown prince to be far away from the capital.",
+        
+        "In Lanka, Ponniyin Selvan and Vandiyadevan are attacked by Pandya Aabathudavigal (a group of devout soldiers in the Pandya dynasty), which is headed by Ravi Dasan, but are rescued by a veiled woman Oomai Rani (who had saved Ponniyin Selvan from drowning in the Ponni river at a young age, thus giving the name). Kundavai meets Aditha Karikalan and tells him to arrive back at the kingdom by arguing that her decision to have Nandini banished was right as she took that decision as a princess, but Aditha Karikalan still adamantly refuses. Meanwhile, Pandya Aabuthudavigal's member meets Mahinda and receives soldiers as help from him in the group's plan to assassinate Ponniyin Selvan. Poonguzhali, realizing that the convoy is sent to apprehend Ponniyin Selvan for political reasons reveals about it to Ponniyin Selvan, Vandiyadevan, Nambi, and others. Ponniyin Selvan makes Vandiyadevan disguise himself as Ponniyin Selvan, and leaves with Poonguzhali, disguised as the mahout to meet the convoy when his well-wishers prevent him from doing so, fearing threat for his life from Pazhuvettaraiyar.",
+        
+        "On the way toward shore, Ponniyin Selvan and Poonguzhali discover that the convoy has been ambushed by Pandya Aabuthudavigal, who apprehends Vandiyadevan, misinterpreting him to be Ponniyin Selvan. Ponniyin Selvan heads towards Pandya Aabuthudavigal's ship to rescue Vandiyadevan and they drown in the sea due to a storm and are consequently, presumed to be dead. The news of Ponniyin Selvan's death reaches Sundara Chozhar, Kundavai, and Aditha Karikalan who are all devastated. Aditha Karikalan swears vengeance against Nandini for his brother's death and heads towards the kingdom to destroy her. Meanwhile, Oomai Rani, revealed to be a lookalike of Nandini, is shown swimming in the sea to find Ponniyin Selvan, to rescue him."]
+    }
+
+    document.getElementById("title").innerHTML = SideBanner.Movie_Name;
+    document.getElementById("content").innerHTML = content.main[0] + "<br><br>" + content.main[1] + "<br><br>" + content.main[2];
+    document.getElementById("sub_head").innerHTML = "Plot";
+    document.getElementById("sub_con").innerHTML = content.plot[0] + "<br><br>" + content.plot[1] + "<br><br>" + content.plot[2] + "<br><br>" + content.plot[3] + "<br><br>" + content.plot[4] + "<br><br>" + content.plot[5];
+
+    let table = document.getElementById("table_cont");
+    let row = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+    cell1.setAttribute("colspan",2);
+    cell1.style.setProperty("text-align","center");
+
+    cell1.innerHTML = SideBanner.Movie_Name;
+
+    row = table.insertRow(1);
+    cell1 = row.insertCell(0);
+    cell1.setAttribute("colspan",2);
+    cell1.style.setProperty("text-align","center");
+
+    var img = document.createElement("IMG")
+    img.setAttribute("src", SideBanner.Film_Poster[0]);
+
+    cell1.appendChild(img);
+
+    row = table.insertRow(2);
+    cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+
+    cell1.innerHTML = "Directed by";
+    cell2.innerHTML = SideBanner.Directed_by;
+
+    row = table.insertRow(3);
+    cell1 = row.insertCell(0);
+    cell2 = row.insertCell(1);
+
+    cell1.innerHTML = "Screenplay";
+    cell2.innerHTML = SideBanner.Screenplay[0] + "<br>"+ SideBanner.Screenplay[1];
+
+    row = table.insertRow(4);
+    cell1 = row.insertCell(0);
+    cell2 = row.insertCell(1);
+
+    cell1.innerHTML = "Based on";
+    cell2.innerHTML = SideBanner.Based_on[0] + "<br>"+ SideBanner.Based_on[1];
+
+    row = table.insertRow(5);
+    cell1 = row.insertCell(0);
+    cell2 = row.insertCell(1);
+
+    cell1.innerHTML = "Produced by";
+    cell2.innerHTML = SideBanner.Produced_by[0] + "<br>"+ SideBanner.Produced_by[1];
+
+    row = table.insertRow(6);
+    cell1 = row.insertCell(0);
+    cell2 = row.insertCell(1);
+
+    cell1.innerHTML = "Starring";
+    cell2.innerHTML = SideBanner.Starring[0] + "<br>"+ SideBanner.Starring[1] + "<br>" + SideBanner.Starring[2] + "<br>"+ SideBanner.Starring[3] + "<br>" + SideBanner.Starring[4] + "<br>"+ SideBanner.Starring[5] + "<br>" + SideBanner.Starring[6] + "<br>"+ SideBanner.Starring[7] + "<br>" + SideBanner.Starring[8] + "<br>"+ SideBanner.Starring[9] + "<br>" + SideBanner.Starring[10] + "<br>"+ SideBanner.Starring[11] + "<br>" + SideBanner.Starring[12] + "<br>"+ SideBanner.Starring[13];
+
+    row = table.insertRow(7);
+    cell1 = row.insertCell(0);
+    cell2 = row.insertCell(1);
+
+    cell1.innerHTML = "Narrated by";
+    cell2.innerHTML = SideBanner.Narrated_by;
+
+    row = table.insertRow(8);
+    cell1 = row.insertCell(0);
+    cell2 = row.insertCell(1);
+
+    cell1.innerHTML = "Cinematography";
+    cell2.innerHTML = SideBanner.Cinematography;
+
+    row = table.insertRow(9);
+    cell1 = row.insertCell(0);
+    cell2 = row.insertCell(1);
+
+    cell1.innerHTML = "Edited by";
+    cell2.innerHTML = SideBanner.Edited_by;
+
+    row = table.insertRow(10);
+    cell1 = row.insertCell(0);
+    cell2 = row.insertCell(1);
+
+    cell1.innerHTML = "Music by";
+    cell2.innerHTML = SideBanner.Music_by;
+
+    row = table.insertRow(11);
+    cell1 = row.insertCell(0);
+    cell2 = row.insertCell(1);
+
+    cell1.innerHTML = "Production companies";
+    cell2.innerHTML = SideBanner.Production_companies[0] + "<br>" + SideBanner.Production_companies[1];
+
+    row = table.insertRow(12);
+    cell1 = row.insertCell(0);
+    cell2 = row.insertCell(1);
+
+    cell1.innerHTML = "Distributed by";
+    cell2.innerHTML = SideBanner.Distributed_by[0] + "<br>" + SideBanner.Distributed_by[1] + "<br>" + SideBanner.Distributed_by[2] + "<br>" + SideBanner.Distributed_by[3] + "<br>" + SideBanner.Distributed_by[4] + "<br>" + SideBanner.Distributed_by[5];
+
+    row = table.insertRow(13);
+    cell1 = row.insertCell(0);
+    cell2 = row.insertCell(1);
+
+    cell1.innerHTML = "Release date";
+    cell2.innerHTML = SideBanner.Release_date;
+
+    row = table.insertRow(14);
+    cell1 = row.insertCell(0);
+    cell2 = row.insertCell(1);
+
+    cell1.innerHTML = "Running time";
+    cell2.innerHTML = SideBanner.Running_time;
+
+    row = table.insertRow(15);
+    cell1 = row.insertCell(0);
+    cell2 = row.insertCell(1);
+
+    cell1.innerHTML = "Country";
+    cell2.innerHTML = SideBanner.Country;
+
+    row = table.insertRow(16);
+    cell1 = row.insertCell(0);
+    cell2 = row.insertCell(1);
+
+    cell1.innerHTML = "Language";
+    cell2.innerHTML = SideBanner.Language;
+
+    row = table.insertRow(17);
+    cell1 = row.insertCell(0);
+    cell2 = row.insertCell(1);
+
+    cell1.innerHTML = "Budget";
+    cell2.innerHTML = SideBanner.Budget;
+
+    row = table.insertRow(18);
+    cell1 = row.insertCell(0);
+    cell2 = row.insertCell(1);
+
+    cell1.innerHTML = "Box_office";
+    cell2.innerHTML = SideBanner.Box_office;
+
+}
