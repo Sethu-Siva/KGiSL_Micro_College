9431b2af35893950817af6560838af33dfeeabf7
Sethu-Siva <sivarocks742@gmail.com>
2023-03-17 02:07:13 +0530

    Save PortFolio

---
+++ personal-portfolio/src/App.js
@@ -0,0 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { NavBar } from './Components/Nav Bar/navBar';
+import { HomePage } from './Components/Home Page/Home';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+            <Route path='/' element={[<NavBar/>,<HomePage/>]}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ personal-portfolio/src/Components/Home Page/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function HomePage()
+{
+    return (
+        <div>
+            
+        </div>
+    );
+}
