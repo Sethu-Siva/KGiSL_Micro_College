4b62627c406dcb2bc54ec55aa436f79814894fa8
Sethu-Siva <sivarocks742@gmail.com>
2023-03-23 19:12:27 +0530

    Save

--- personal-portfolio/src/Components/Home Page/Home.js
+++ personal-portfolio/src/Components/Home Page/Home.js
@@ -1,10 +1,17 @@
 import React from "react";
+import './Styles.css';
 
 export function HomePage()
 {
     return (
         <div>
-            
+            <div className="container-fluid HomePage">
+                <div className="row">
+                <div className="col-12 HomePageDiv">
+                        <h1>Sethu Siva</h1>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
